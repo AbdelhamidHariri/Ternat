8f07e8bd677d459c534026d2003ca479ec78be06
ABHA1 <abha1@ferring.com>
2024-03-17 00:58:42 +0100

    added migration and imporved generation

--- migrate.config.js
+++ migrate.config.js
@@ -1,7 +1,7 @@
 export default {
-    database: "",
-    host: "",
-    port: "",
-    username: "",
-    password: "",
+    database: process.env["DB_DATABASE"],
+    host: process.env["DB_HOST"],
+    port: process.env["DB_PORT"],
+    username: process.env["DB_USERNAME"],
+    password: process.env["DB_PASSWORD"],
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { red, yellow } from "./lib/chalk.js";
 import { NO_OPERATION_ERROR } from "./lib/cli-errors.js";
 import generate from "./operations/generate.js";
 import init from "./operations/init.js";
+import migrate from "./operations/migrate.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const operation = process.argv[2];
 const dirname = process.env["PWD"];
@@ -17,6 +20,8 @@ switch (operation) {
         generate(dirname, name);
         break;
     case "migrate":
+        migrate(dirname);
+        break;
     case "reset":
     default:
         console.log(red(NO_OPERATION_ERROR.message));
