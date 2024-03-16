3128c97ee3c1ad03d940ca53bb6dec9aec5c171e
ABHA1 <abha1@ferring.com>
2024-03-16 22:18:00 +0100

    fixed bin executable not working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
+
 import { error, warning } from "./lib/chalk.js";
 import { NO_OPERATION_ERROR } from "./lib/cli-errors.js";
 import generate from "./operations/generate.js";
 import init from "./operations/init.js";
 
 const operation = process.argv[2];
-const dirname = process.argv[1].split("/").slice(0, -1).join("/");
+const dirname = process.env["PWD"];
 const name = process.argv[3];
 
 switch (operation) {
